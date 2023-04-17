a736ff131dd2e326f2890066f944c30ba9008cee
gwonyejun <kyjuni1059@gmail.com>
2023-04-17 21:00:05 +0900

    덧셈 문자열 구분

---
+++ app.js
@@ -0,0 +1,26 @@
+function examOne(first, second) {
+  if ("string" === typeof(first) || "string"  === typeof(second)) {
+   return console.log("문자열 데이터를 입력하셨습니다.");
+
+  }
+  else{
+    return console.log(typeof(second));
+  }
+
+
+}
+function examTwo(first, second, third) {}
+function examThree(str) {}
+function examFour(array) {}
+function examFive(array) {}
+
+console.log(examOne('2', 1));
+// ! 덧셈 로직 만들기
+console.log(examTwo(2, 3, 4));
+// !곱셈 로직 만들기
+console.log(examThree("hello"));
+// ! 문자열 길이 구하기
+console.log(examFour([3, 1, 4, 2]));
+// ! 최솟값 판별하기
+console.log(examFive([1, 2, 3]));
+// ! 배열값 누산(총합)하기
