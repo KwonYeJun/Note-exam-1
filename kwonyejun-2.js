f5dbc0e57b58bcac817db0074163851e93a0d736
gwonyejun <kyjuni1059@gmail.com>
2023-04-18 20:36:01 +0900

    완성

--- kwonyejun-2.js
+++ kwonyejun-2.js
@@ -13,9 +13,12 @@ function exampleOne(count, min, max) {
     // * 난수를 발생 시켜 toFixed() 메서드를 사용하여 소수자리를 2째자리까지 출력 하게 하였다.
   }
   return exampleArr;
+// ! exampleTwo()에 array값을 넣어 줄때 사용 한다.
 }
 
 function exampleTwo(array, divideNumber) {
+  // ! exampleOne()에서 리턴된 값이 array로 들어 온다.
+
   let exampleJObject = {
     a: [],
     b: [],
@@ -23,15 +26,15 @@ function exampleTwo(array, divideNumber) {
 
   for(let i =0; i< array.length; i++){
     if(array[i] > divideNumber){
-      exampleJObject.a = array[i];
+      exampleJObject.a = Number(array[i]);
     }
     else{
-      exampleJObject.b = array[i];
+      exampleJObject.b = Number(array[i]);
 
     }
   }
 
-  return  exampleJObject;
+  return exampleJObject;
 }
 function exampleFour(array) {
   let value = 0;
@@ -47,16 +50,22 @@ function exampleFive(objectOne, objectTwo) {
 
   let One = exampleFour(objectOne);
   let Two = exampleFour(objectTwo);
-
-  return Two;
+  value =  One + Two;
+  return "One " +'+'+' Two' +' = '+ value;
 }
 
 const { count, min, max } = basicData; // 객체 디스트럭처링
 const result = exampleOne(count, min, max);
 // console.log(result);
+// console.log(exampleTwo(result,basicData.divideNumber))
+// // console.log(basicData.basicDataKeys[1]);
 
-// console.log(basicData.basicDataKeys[1]);
-// console.log(exampleFive(basicData, exampleTwo(result, basicData.divideNumber)));
-// console.log(exampleFive(basicData, exampleTwo(result, basicData.divideNumber)));
+// console.log(exampleFour(exampleTwo(result,basicData.divideNumber)));
 
-console.log(exampleTwo(result,basicData.divideNumber))
+
+console.log(exampleFive(basicData, exampleTwo(result, basicData.divideNumber)));
+
+
+
+
+// console.log(exampleTwo(result,basicData.divideNumber))
