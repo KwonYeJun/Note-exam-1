55800ffb0e8cb8f104f9e3b2b6c3f1604d110bcd
gwonyejun <kyjuni1059@gmail.com>
2023-04-18 20:14:42 +0900

    four() 함수에 객체 벨류값 더 하는 로직 작성

--- kwonyejun-2.js
+++ kwonyejun-2.js
@@ -2,13 +2,15 @@ let basicData = {
   count: 3,
   min: 1,
   max: 10,
+  divideNumber: 5,
 };
 
 function exampleOne(count, min, max) {
   let exampleArr = [];
   for (let i = 0; i < count; i++) {
-    const randomNumber = (Math.random() * (max - min)) + min;
+    const randomNumber = Math.random() * (max - min) + min;
     exampleArr[i] = randomNumber.toFixed(2);
+    // * 난수를 발생 시켜 toFixed() 메서드를 사용하여 소수자리를 2째자리까지 출력 하게 하였다.
   }
   return exampleArr;
 }
@@ -21,12 +23,21 @@ function exampleTwo(array, divideNumber) {
   return exampleJObject;
 }
 function exampleFour(array) {
-  let value;
+  let value = 0;
+  for (let key in array) {
+    if (typeof array[key] === "number") {
+      value += array[key];
+    }
+  }
   return value;
 }
 function exampleFive(objectOne, objectTwo) {
   let value;
-  return value;
+
+  let One = exampleFour(objectOne);
+  let Two = exampleFour(objectTwo);
+
+  return One;
 }
 
 const { count, min, max } = basicData; // 객체 디스트럭처링
@@ -34,4 +45,5 @@ const result = exampleOne(count, min, max);
 console.log(result);
 
 // console.log(basicData.basicDataKeys[1]);
-// console.log(exampleFive(basicData, exampleTwo()));
+// console.log(exampleFive(basicData, exampleTwo(result, basicData.divideNumber)));
+console.log(exampleFour(basicData));
