362ad9bd2c3ed0a8f6ec97ef828d6977a02a4b1c
gwonyejun <kyjuni1059@gmail.com>
2023-04-20 21:27:52 +0900

    exampeTree() 함수에서 무엇을 요구 하는지 보고 원하는 조건들을 하나씩 해결 해보다 보니 문제가 원하는게 무엇인지 보였다.

--- exam-4test.js
+++ exam-4test.js
@@ -1,4 +1,20 @@
-function test(){
+function arrayLengthEven(array) {
+  if (array.length % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+let basicData = [4, 6, 23, 5, 94, 35, 44];
+
+if(arrayLengthEven(basicData) === false){
+  basicData.push(10);
+  console.log(basicData.length);
+  // push()를 사용하여 인덱스 10을 추가 하였따.
+ }  
+else{
+  console.log(arrayLengthEven(basicData))
 
 }
-console.log(typeof(test));
